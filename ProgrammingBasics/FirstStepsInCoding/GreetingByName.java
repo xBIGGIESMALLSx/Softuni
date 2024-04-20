5c72ea8eb6fc495fc96ec23533846811cede1c3a
Marin Karpachev <167113624+xBIGGIESMALLSx@users.noreply.github.com>
2024-04-20 17:57:37 +0300

    Update GreetingByName.java

--- ProgrammingBasics/FirstStepsInCoding/GreetingByName.java
+++ ProgrammingBasics/FirstStepsInCoding/GreetingByName.java
@@ -1,17 +1,9 @@
 import java.util.Scanner;
 
 public class GreetingByName {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         String name = scanner.nextLine();
-        String lastName = scanner.nextLine();
-        int age = Integer.parseInt(scanner.nextLine());
-        String city = scanner.nextLine();
-
-        System.out.printf("You are %s %s, a %d-years old person from %s", name,lastName, age, city);
-
+        System.out.printf("Hello, %s!", name);
     }
 }
