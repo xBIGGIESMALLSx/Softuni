e6d7a63c435ef5063d42e62f553e0fed76536789
Marin Karpachev <167113624+xBIGGIESMALLSx@users.noreply.github.com>
2024-04-20 18:54:36 +0300

    Create CalculatorDeposit.java

---
+++ ProgrammingBasics/FirstStepsInCoding-Exercise/CalculatorDeposit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CalculatorDeposit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double deposit = Double.parseDouble(scanner.nextLine());
+        int months = Integer.parseInt(scanner.nextLine());
+        double percent = Double.parseDouble(scanner.nextLine());
+        double sum = deposit + months * ((deposit * percent/100) / 12);
+        System.out.println(sum);
+
+    }
+}
